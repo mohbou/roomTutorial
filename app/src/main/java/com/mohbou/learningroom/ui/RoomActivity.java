4b999421a02c369f185cd303469427dc0b6752b7
mohbou <a.bourkaib@gmail.com>
2018-03-03 10:54:14 -0500

    initial commit

--- app/src/main/java/com/mohbou/learningroom/ui/RoomActivity.java
+++ app/src/main/java/com/mohbou/learningroom/ui/RoomActivity.java
@@ -1,9 +1,11 @@
-package com.mohbou.learningroom;
+package com.mohbou.learningroom.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
+import com.mohbou.learningroom.R;
 import com.mohbou.learningroom.database.AppDatabase;
 import com.mohbou.learningroom.model.User;
 
@@ -15,11 +17,15 @@ public class RoomActivity extends AppCompatActivity {
 
 
     private AppDatabase database;
+
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
         database = AppDatabase.getInstance(this);
+
+        recyclerView = findViewById(R.id.recyclerView);
 
         int userCount = database.userDao().usersCount();
 
---
+++ app/src/main/java/com/mohbou/learningroom/ui/ViewAdapter.java
@@ -0,0 +1,24 @@
+package com.mohbou.learningroom.ui;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+
+public class ViewAdapter extends RecyclerView.Adapter<ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
